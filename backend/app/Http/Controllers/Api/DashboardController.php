e424d497ee6751ea21cc644d5449b31dadf41c70
Abhinav <abhinavpal8960@gmail.com>
2026-04-04 22:23:35 +0530

    new changes

--- backend/app/Http/Controllers/Api/DashboardController.php
+++ backend/app/Http/Controllers/Api/DashboardController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\FinancialRecord;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     //
-    public function index(){
-        
+    public function index()
+    {
+        $users = User::orderBy('created_at', 'DESC')->get();
+        $categories = Category::orderBy('created_at', 'DESC')->get();
+        $finaancial_records = FinancialRecord::orderBy('created_at', 'DESC')->get();
+
+        return response()->json([
+            'status' => true,
+            'users' => $users,
+            'finaancial_records' => $finaancial_records
+        ]);
     }
 }
